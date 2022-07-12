b6d04a597efb8216b13012d2fe47a755a02233d8
jsdevin <jsdevin@163.com>
2022-07-12 21:30:32 +0800

    添加域名

---
+++ assets/项目笔记.html.6b2f53fc.js
@@ -0,0 +1,37 @@
+import{_ as t,r as p,o,c,a as n,b as e,d as s,e as l}from"./app.b57af783.js";const i={},u=n("h1",{id:"\u54CD\u5E94\u5F0F\u8BBE\u8BA1",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#\u54CD\u5E94\u5F0F\u8BBE\u8BA1","aria-hidden":"true"},"#"),s(" \u54CD\u5E94\u5F0F\u8BBE\u8BA1")],-1),r=n("h2",{id:"\u9879\u76EE\u80CC\u666F",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#\u9879\u76EE\u80CC\u666F","aria-hidden":"true"},"#"),s(" \u9879\u76EE\u80CC\u666F")],-1),d=s("\u4F9D\u636E\u5C4F\u5E55\u5C3A\u5BF8\u7684\u4E0D\u540C\u663E\u793A\u4E0D\u540C\u7684\u80CC\u666F\u989C\u8272\u3002"),k=n("br",null,null,-1),v={href:"https://media.devin.ren",target:"_blank",rel:"noopener noreferrer"},m=s("\u7EBF\u4E0A\u9884\u89C8"),b=l(`<h2 id="\u4EE3\u7801\u5B9E\u73B0" tabindex="-1"><a class="header-anchor" href="#\u4EE3\u7801\u5B9E\u73B0" aria-hidden="true">#</a> \u4EE3\u7801\u5B9E\u73B0</h2><div class="language-html ext-html line-numbers-mode"><pre class="language-html"><code>  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>style</span><span class="token punctuation">&gt;</span></span><span class="token style"><span class="token language-css">
+    <span class="token selector">.demo</span> <span class="token punctuation">{</span>
+      <span class="token property">background-color</span><span class="token punctuation">:</span> greenyellow<span class="token punctuation">;</span>
+      <span class="token property">width</span><span class="token punctuation">:</span> 1200px<span class="token punctuation">;</span>
+      <span class="token property">height</span><span class="token punctuation">:</span> 500px<span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+
+    <span class="token atrule"><span class="token rule">@media</span> screen <span class="token keyword">and</span> <span class="token punctuation">(</span><span class="token property">max-width</span><span class="token punctuation">:</span> 1000px<span class="token punctuation">)</span></span> <span class="token punctuation">{</span>
+      <span class="token selector">.demo</span> <span class="token punctuation">{</span>
+        <span class="token property">background-color</span><span class="token punctuation">:</span> red<span class="token punctuation">;</span>
+      <span class="token punctuation">}</span>
+    <span class="token punctuation">}</span>
+
+    <span class="token atrule"><span class="token rule">@media</span> screen <span class="token keyword">and</span> <span class="token punctuation">(</span><span class="token property">max-width</span><span class="token punctuation">:</span> 800px<span class="token punctuation">)</span></span> <span class="token punctuation">{</span>
+      <span class="token selector">.demo</span> <span class="token punctuation">{</span>
+        <span class="token property">background-color</span><span class="token punctuation">:</span> blue<span class="token punctuation">;</span>
+      <span class="token punctuation">}</span>
+    <span class="token punctuation">}</span>
+
+    <span class="token atrule"><span class="token rule">@media</span> screen <span class="token keyword">and</span> <span class="token punctuation">(</span><span class="token property">max-width</span><span class="token punctuation">:</span> 500px<span class="token punctuation">)</span></span> <span class="token punctuation">{</span>
+      <span class="token selector">.demo</span> <span class="token punctuation">{</span>
+        <span class="token property">background-color</span><span class="token punctuation">:</span> pink<span class="token punctuation">;</span>
+      <span class="token punctuation">}</span>
+    <span class="token punctuation">}</span>
+
+    <span class="token atrule"><span class="token rule">@media</span> screen <span class="token keyword">and</span> <span class="token punctuation">(</span><span class="token property">max-width</span><span class="token punctuation">:</span> 300px<span class="token punctuation">)</span></span> <span class="token punctuation">{</span>
+      <span class="token selector">.demo</span> <span class="token punctuation">{</span>
+        <span class="token property">background-color</span><span class="token punctuation">:</span> purple<span class="token punctuation">;</span>
+      <span class="token punctuation">}</span>
+    <span class="token punctuation">}</span>
+  </span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>style</span><span class="token punctuation">&gt;</span></span>
+
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>body</span><span class="token punctuation">&gt;</span></span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span> <span class="token attr-name">class</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>demo<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span>\u8C03\u7528f12\u7F29\u5C0F\u5C3A\u5BF8\u5373\u53EF\u4F7F\u5F97\u80CC\u666F\u53D8\u8272<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span>
+  <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>body</span><span class="token punctuation">&gt;</span></span>
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),h={href:"https://developer.mozilla.org/zh-CN/docs/Learn/CSS/CSS_layout/Responsive_Design",target:"_blank",rel:"noopener noreferrer"},_=s("\u54CD\u5E94\u5F0F\u8BBE\u8BA1\u53C2\u8003\u8D44\u6599");function g(y,x){const a=p("ExternalLinkIcon");return o(),c("div",null,[u,r,n("p",null,[d,k,n("a",v,[m,e(a)])]),b,n("p",null,[n("a",h,[_,e(a)])])])}var w=t(i,[["render",g],["__file","\u9879\u76EE\u7B14\u8BB0.html.vue"]]);export{w as default};
